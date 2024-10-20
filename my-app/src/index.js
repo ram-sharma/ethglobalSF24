9ca059b942a1fcbdc0b3260ae50981d7755a2e0c
Ram Sharma <ramsharma@Rams-MacBook-Pro.local>
2024-10-20 00:04:55 -0700

    updates

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Optional: You can create this for styling, or ignore this line if not needed
-import App from './App'; // This is where your main component lives
+import App from './App.js'; // This is where your main component lives
 
 ReactDOM.render(
   <React.StrictMode>
