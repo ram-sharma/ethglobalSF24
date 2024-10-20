d53ce47419336b96828a94dae5b69a6019f6b4d0
Ram Sharma <ramsharma@Rams-MacBook-Pro.local>
2024-10-19 23:55:20 -0700

    create react app wrapper

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional: You can create this for styling, or ignore this line if not needed
+import App from './App'; // This is where your main component lives
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
